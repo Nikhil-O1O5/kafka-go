c2a50353403e67e98e67a7c06d9bd3b6be363ee6
Nikhil Basutkar <nikhilbasutkar01@gmail.com>
2026-09-22 16:02:00 +0530

    feat: update producer and main to use structured Event messages

--- cmd/main.go
+++ cmd/main.go
@@ -1,45 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"time"
+
 	"github.com/Nikhil-O1O5/kafka-go/internal/consumer"
 	"github.com/Nikhil-O1O5/kafka-go/internal/producer"
-	"time"
+	"github.com/Nikhil-O1O5/kafka-go/internal/repo"
+	"github.com/Nikhil-O1O5/kafka-go/internal/shared"
 )
 
 type Server struct {
 	producer *producer.KafkaProducer
 	consumer *consumer.KafkaConsumer
-	msgCH    chan string
+	msgCH    chan *shared.Message
 }
 
 func NewServer() *Server {
-	msgCH := make(chan string, 64)
+	msgCH := make(chan *shared.Message, 64)
 	c, err := consumer.NewKafkaConsumer(msgCH)
 	if err != nil {
 		panic(err)
 	}
-	p := producer.NewKafkaProducer("")
 	return &Server{
-		producer: p,
+		producer: producer.NewKafkaProducer(""),
 		consumer: c,
 		msgCH:    msgCH,
 	}
 }
 
 func (s *Server) produceMsg() {
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
-	id := 0
-	for t := range ticker.C {
-		msg := fmt.Sprintf("hello from kafka, msgID = %d, ts = %s", id, t.Format("15:20:20"))
-		s.producer.Produce(msg)
-		id++
+	for range ticker.C {
+		event := repo.NewEvent()
+		b, err := json.Marshal(event)
+		if err != nil {
+			fmt.Printf("marshal error: %v\n", err)
+			continue
+		}
+		s.producer.Produce(b)
 	}
 }
 
-func (s *Server) handleMsg(msg string) {
-	fmt.Printf("received msg = %s\n", msg)
+func (s *Server) handleMsg(msg *shared.Message) {
+	fmt.Printf("received offset=%d event_id=%s\n", msg.Metadata.Offset, msg.Event.EventId)
+	s.consumer.MarkAsComplete(msg.Metadata)
 }
 
 func main() {
